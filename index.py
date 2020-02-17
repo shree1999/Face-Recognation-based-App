43c403530c1b029115ebab3e2c8aee1903e5533a
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-02-17 23:20:24 +0530

    Added user input and validations

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 import pandas as pd
 import cv2
 import os
@@ -14,5 +15,90 @@ root.title("Face Attendance Management System-FAMS")
 
 root.geometry("1280x720")
 root.configure(background="snow")
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.iconbitmap('./logo.ico')
+
+
+# function to provide closing of FAMS
+def onClosingApplication():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+
+root.protocol(name="WM_DELETE_WINDOW", func=onClosingApplication)
+
+# function to validate whether the roll number is numeric or not
+def entryValidation(element, acttype):
+    if acttype == '1':
+        if not element.isdigit():
+            return False
+    return True
+
+
+# Creating all the required enteries for our gui app
+topic = tk.Label(
+        root, 
+        text="Face-Recognition-Based-Attendance-Management-System",
+        bg="cyan",
+        fg="black",
+        width=50,
+        height=3,
+        font=('times', 30, 'italic bold')
+    )
+topic.place(x=50, y=20)
+
+notifications = tk.Label(
+        root,
+        text="All Things are Set",
+        bg="green",
+        fg="white",
+        width=15,
+        height=3,
+        font=('times', 17, 'bold')
+    )
+
+label1 = tk.Label(
+        root,
+        text="Enter Roll-Number",
+        bg="deep pink", 
+        fg="black", 
+        width=20,
+        height=2,
+        font=('times', 15, 'bold')
+    )
+label1.place(x=150, y=200)
+
+text1 = tk.Entry(
+        root,
+        bg="yellow",
+        fg="red",
+        font=('times', 25, 'bold'),
+        validate="key",
+        width=20
+    )
+text1['validatecommand'] = (text1.register(func=entryValidation), '%P', '%d')
+text1.place(x=500, y=210)
+
+label2 = tk.Label(
+        root,
+        text="Enter Name",
+        bg="deep pink", 
+        fg="black", 
+        width=20,
+        height=2,
+        font=('times', 15, 'bold')
+    )
+
+label2.place(x=150, y=290)
+
+text2 = tk.Entry(
+        root,
+        bg="yellow",
+        fg="red",
+        font=('times', 25, 'bold'),
+        width=20
+    )
+text2.place(x=500, y=300)
 
 root.mainloop()
