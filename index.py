5da477cbafd056f1de6c801704981465bd4f91a1
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-02-17 22:20:03 +0530

    Added main index file with important libraries

---
+++ index.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+import pandas as pd
+import cv2
+import os
+import csv
+import numpy as np
+from PIL import Image, ImageTk
+import datetime
+import time
+
+root = tk.Tk()
+root.title("Face Attendance Management System-FAMS")
+
+root.geometry("1280x720")
+root.configure(background="snow")
+
+root.mainloop()
